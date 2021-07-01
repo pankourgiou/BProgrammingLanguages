e63d515516ae96b3f91b051f41d83da38cdb2893
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-07-01 08:39:45 +0300

    Update and rename artificialove.emojic to B_ProgrammingLanguages.py

---
+++ B_ProgrammingLanguages.py
@@ -0,0 +1,3 @@
+
+for _ in range(1000000):
+    print("B B Babbage Ballerina Bash BASIC Batch file (Windows/MS-DOS) bc (basic calculator) BCPL BeanShell Bertrand BETA BLISS Blockly BlooP Boo Boomerang Bosque")
