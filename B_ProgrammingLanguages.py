bbecaaf2210902db3fd938c6fc22ef4fce05fabc
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2023-08-25 18:19:27 +0300

    Update B_ProgrammingLanguages.py

--- B_ProgrammingLanguages.py
+++ B_ProgrammingLanguages.py
@@ -1,3 +1,15 @@
+import tkinter as tk
 
-for _ in range(1000000):
-    print("B B Babbage Ballerina Bash BASIC Batch file (Windows/MS-DOS) bc (basic calculator) BCPL BeanShell Bertrand BETA BLISS Blockly BlooP Boo Boomerang Bosque")
+root= tk.Tk()
+
+canvas1 = tk.Canvas(root, width = 300, height = 300)
+canvas1.pack()
+
+def hello ():  
+    label1 = tk.Label(root, text= 'B B Babbage Ballerina Bash BASIC Batch file (Windows/MS-DOS) bc (basic calculator) BCPL BeanShell Bertrand BETA BLISS Blockly BlooP Boo Boomerang Bosque', fg='Red', font=('KAOTICAN', 12, 'bold'))
+    canvas1.create_window(150, 200, window=label1)
+    
+button1 = tk.Button(text='Click Me', command=hello, bg='brown',fg='white')
+canvas1.create_window(150, 150, window=button1)
+
+root.mainloop()
